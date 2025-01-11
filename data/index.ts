1779dbd5e1df9895e7921af892e1576058c7f7e2
IITI-tushar <019saxenatushar@gmail.com>
2025-01-11 20:22:49 +0530

    added-portfolio-website

---
+++ data/index.ts
@@ -0,0 +1,134 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I can collaborate globally with clients and organizations to build and enhance their websites.",
+    description: "",
+    className:
+      "lg:col-span-3 md:col-span-3 md:row-span-2 lg:top-0 lg:py-5 lg:h-[50vh] z-150",
+    imgClassName: "",
+    titleClassName: "justify-end",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Exploring the vast potential of Machine Learning....",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Building seamless server-side solutions.",
+    description: "Passionate for Backend",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Quiz APP",
+    des: "Built a Quiz app using MERN where user can create and take quizzes",
+    img: "/quiz-app.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio Website",
+    des: "Built an Responsive and mordern looking portfolio website using Next.js",
+    img: "/portfolio.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "/",
+  },
+  {
+    id: 3,
+    title: "Meme Generator Website",
+    des: "Built an Meme Generator website where user can create memes and download them",
+    img: "/meme-gen.png",
+    iconLists: ["/re.svg"],
+    link: "/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Gymkhana WebD team of IIT Indore",
+    desc: "Currently working on Humanities and Social Science Website",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Open Source Developer",
+    desc: "Fixed Bugs, added features, updated Docs at open source organisations like AsyncAPI, Palisadoes foundation and Rocket.Chat",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
