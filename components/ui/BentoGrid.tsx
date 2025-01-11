923b4f563e5b6aa427b56007e2067d0e0e9a2744
IITI-tushar <019saxenatushar@gmail.com>
2025-01-11 21:03:02 +0530

    updated again

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -11,6 +11,7 @@ import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 import { motion } from "framer-motion";
+import { ClientWrapper } from "./ClientWrapper";
 
 export const BentoGrid = ({
   className,
@@ -91,152 +92,154 @@ export const BentoGridItem = ({
   };
 
   return (
-    <div
-      className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
-        className
-      )}
-      style={{
-        background: "rgb(2,0,36)",
-        backgroundColor:
-          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(122,106,192,1) 100%, rgba(0,212,255,1) 100%)",
-      }}
-    >
-      <div className={`${id === 6 && "flex justify-center"} h-full`}>
-        <div className="w-full h-full absolute">
-          {img && (
-            <img
-              src={img}
-              alt={img}
-              className={cn(imgClassName, "object-cover object-center ")}
-            />
-          )}
-        </div>
-        <div
-          className={`absolute right-0 -bottom-5 ${
-            id === 5 && "w-full opacity-80"
-          } `}
-        >
-          {spareImg && (
-            <img
-              src={spareImg}
-              alt={spareImg}
-              className="object-cover object-center w-full h-full"
-            />
-          )}
-        </div>
-        {id === 6 && (
-          <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-2xl text-center md:text-4xl lg:text-7xl"></div>
-          </BackgroundGradientAnimation>
+    <ClientWrapper>
+      <div
+        className={cn(
+          "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+          className
         )}
-
-        <div
-          className={cn(
-            titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-          )}
-        >
-          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-            {description}
+        style={{
+          background: "rgb(2,0,36)",
+          backgroundColor:
+            "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(122,106,192,1) 100%, rgba(0,212,255,1) 100%)",
+        }}
+      >
+        <div className={`${id === 6 && "flex justify-center"} h-full`}>
+          <div className="w-full h-full absolute">
+            {img && (
+              <img
+                src={img}
+                alt={img}
+                className={cn(imgClassName, "object-cover object-center ")}
+              />
+            )}
           </div>
           <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            className={`absolute right-0 -bottom-5 ${
+              id === 5 && "w-full opacity-80"
+            } `}
           >
-            {title}
-          </div>
-
-          {id === 1 && <GridGlobe />}
-
-          {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 px-5">
-              {/* Left List with Infinite Scrolling */}
-              <motion.div
-                className="flex flex-col gap-3 md:gap-3 lg:gap-8"
-                variants={infiniteScrollVariant}
-                animate="animate"
-                transition={{
-                  duration: 6, // Adjust scrolling speed
-                  ease: "linear", // Smooth constant scroll
-                  repeat: Infinity, // Loop infinitely
-                }}
-              >
-                {leftLists.map((item, i) => (
-                  <span
-                    key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-                {/* Append items to make scroll seamless */}
-                {leftLists.map((item, i) => (
-                  <span
-                    key={`repeat-left-${i}`}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-              </motion.div>
-
-              {/* Right List with Infinite Scrolling */}
-              <motion.div
-                className="flex flex-col gap-3 md:gap-3 lg:gap-8"
-                variants={infiniteScrollVariant}
-                animate="animate"
-                transition={{
-                  duration: 6, // Adjust scrolling speed
-                  ease: "linear", // Smooth constant scroll
-                  repeat: Infinity, // Loop infinitely
-                }}
-              >
-                {rightLists.map((item, i) => (
-                  <span
-                    key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-                {/* Append items to make scroll seamless */}
-                {rightLists.map((item, i) => (
-                  <span
-                    key={`repeat-right-${i}`}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-              </motion.div>
-            </div>
-          )}
-          {id === 6 && (
-            <div className="mt-5 relative">
-              <div
-                className={`absolute -bottom-5 right-0 ${
-                  copied ? "block" : "block"
-                }`}
-              >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
-                <Lottie options={defaultOptions} height={200} width={400} />
-              </div>
-
-              <MagicButton
-                title={copied ? "Email is Copied!" : "Copy my email address"}
-                icon={<IoCopyOutline />}
-                position="left"
-                handleClick={handleCopy}
-                otherClasses="!bg-[#161A31]"
+            {spareImg && (
+              <img
+                src={spareImg}
+                alt={spareImg}
+                className="object-cover object-center w-full h-full"
               />
-            </div>
+            )}
+          </div>
+          {id === 6 && (
+            <BackgroundGradientAnimation>
+              <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-2xl text-center md:text-4xl lg:text-7xl"></div>
+            </BackgroundGradientAnimation>
           )}
+
+          <div
+            className={cn(
+              titleClassName,
+              "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            )}
+          >
+            <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+              {description}
+            </div>
+            <div
+              className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            >
+              {title}
+            </div>
+
+            {id === 1 && <GridGlobe />}
+
+            {id === 3 && (
+              <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 px-5">
+                {/* Left List with Infinite Scrolling */}
+                <motion.div
+                  className="flex flex-col gap-3 md:gap-3 lg:gap-8"
+                  variants={infiniteScrollVariant}
+                  animate="animate"
+                  transition={{
+                    duration: 6, // Adjust scrolling speed
+                    ease: "linear", // Smooth constant scroll
+                    repeat: Infinity, // Loop infinitely
+                  }}
+                >
+                  {leftLists.map((item, i) => (
+                    <span
+                      key={i}
+                      className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    >
+                      {item}
+                    </span>
+                  ))}
+                  {/* Append items to make scroll seamless */}
+                  {leftLists.map((item, i) => (
+                    <span
+                      key={`repeat-left-${i}`}
+                      className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </motion.div>
+
+                {/* Right List with Infinite Scrolling */}
+                <motion.div
+                  className="flex flex-col gap-3 md:gap-3 lg:gap-8"
+                  variants={infiniteScrollVariant}
+                  animate="animate"
+                  transition={{
+                    duration: 6, // Adjust scrolling speed
+                    ease: "linear", // Smooth constant scroll
+                    repeat: Infinity, // Loop infinitely
+                  }}
+                >
+                  {rightLists.map((item, i) => (
+                    <span
+                      key={i}
+                      className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    >
+                      {item}
+                    </span>
+                  ))}
+                  {/* Append items to make scroll seamless */}
+                  {rightLists.map((item, i) => (
+                    <span
+                      key={`repeat-right-${i}`}
+                      className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+          lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    >
+                      {item}
+                    </span>
+                  ))}
+                </motion.div>
+              </div>
+            )}
+            {id === 6 && (
+              <div className="mt-5 relative">
+                <div
+                  className={`absolute -bottom-5 right-0 ${
+                    copied ? "block" : "block"
+                  }`}
+                >
+                  {/* <img src="/confetti.gif" alt="confetti" /> */}
+                  <Lottie options={defaultOptions} height={200} width={400} />
+                </div>
+
+                <MagicButton
+                  title={copied ? "Email is Copied!" : "Copy my email address"}
+                  icon={<IoCopyOutline />}
+                  position="left"
+                  handleClick={handleCopy}
+                  otherClasses="!bg-[#161A31]"
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </ClientWrapper>
   );
 };
--- components/ui/CanvasRevealEffect.tsx
+++ components/ui/CanvasRevealEffect.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
 import * as THREE from "three";
+import { ClientWrapper } from "./ClientWrapper";
 
 export const CanvasRevealEffect = ({
   animationSpeed = 0.4,
@@ -24,27 +25,31 @@ export const CanvasRevealEffect = ({
   showGradient?: boolean;
 }) => {
   return (
-    <div className={cn("h-full relative bg-white w-full", containerClassName)}>
-      <div className="h-full w-full">
-        <DotMatrix
-          colors={colors ?? [[0, 255, 255]]}
-          dotSize={dotSize ?? 3}
-          opacities={
-            opacities ?? [0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1]
-          }
-          shader={`
+    <ClientWrapper>
+      <div
+        className={cn("h-full relative bg-white w-full", containerClassName)}
+      >
+        <div className="h-full w-full">
+          <DotMatrix
+            colors={colors ?? [[0, 255, 255]]}
+            dotSize={dotSize ?? 3}
+            opacities={
+              opacities ?? [0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, 1]
+            }
+            shader={`
               float animation_speed_factor = ${animationSpeed.toFixed(1)};
               float intro_offset = distance(u_resolution / 2.0 / u_total_size, st2) * 0.01 + (random(st2) * 0.15);
               opacity *= step(intro_offset, u_time * animation_speed_factor);
               opacity *= clamp((1.0 - step(intro_offset + 0.1, u_time * animation_speed_factor)) * 1.25, 1.0, 1.25);
             `}
-          center={["x", "y"]}
-        />
+            center={["x", "y"]}
+          />
+        </div>
+        {showGradient && (
+          <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-[84%]" />
+        )}
       </div>
-      {showGradient && (
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-[84%]" />
-      )}
-    </div>
+    </ClientWrapper>
   );
 };
 
