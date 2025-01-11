c5b9808a16923a638903fac71dd7c47d6c9e1fdc
IITI-tushar <019saxenatushar@gmail.com>
2025-01-12 00:22:25 +0530

    update

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,24 +5,21 @@ import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid className="w-full py-20">
-        Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-        vitae id rerum in consequuntur qui veniam adipisci, deserunt inventore
-        quia doloribus enim, dolore dolor. Corporis sint ex dolorem unde dolore!
-        {/* {gridItems.map((item, i) => (
-          // <BentoGridItem
-          //   id={item.id}
-          //   key={i}
-          //   title={item.title}
-          //   description={item.description}
-          //   className={item.className}
-          //   img={item.img}
-          //   imgClassName={item.imgClassName}
-          //   titleClassName={item.titleClassName}
-          //   spareImg={item.spareImg}
-          // />
-        ))} */}
-      </BentoGrid>
+      {/* <BentoGrid className="w-full py-20"> */}
+      {gridItems.map((item, i) => (
+        <BentoGridItem
+          id={item.id}
+          key={i}
+          title={item.title}
+          description={item.description}
+          className={item.className}
+          img={item.img}
+          imgClassName={item.imgClassName}
+          titleClassName={item.titleClassName}
+          spareImg={item.spareImg}
+        />
+      ))}
+      {/* </BentoGrid> */}
     </section>
   );
 };
