312a11d6ced29c4f96091601a6c3524534495929
IITI-tushar <019saxenatushar@gmail.com>
2025-01-12 00:48:29 +0530

    update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -33,203 +33,203 @@ export const BentoGrid = ({
 };
 
 // BentoGridItem is the individual grid item with dynamic content
-// export const BentoGridItem = ({
-//   className,
-//   id,
-//   title,
-//   description,
-//   img,
-//   imgClassName,
-//   titleClassName,
-//   spareImg,
-// }: {
-//   className?: string;
-//   id: number;
-//   title?: string | React.ReactNode;
-//   description?: string | React.ReactNode;
-//   img?: string;
-//   imgClassName?: string;
-//   titleClassName?: string;
-//   spareImg?: string;
-// }) => {
-//   // List of tech stacks
-//   const leftLists = [
-//     "ReactJS",
-//     "Express",
-//     "Typescript",
-//     "C++",
-//     "Python",
-//     "Node.js",
-//   ];
-//   const rightLists = [
-//     "Docker",
-//     "NextJS",
-//     "GraphQL",
-//     "JavaScript",
-//     "GitHub",
-//     "MongoDB",
-//   ];
+export const BentoGridItem = ({
+  className,
+  id,
+  title,
+  description,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  className?: string;
+  id: number;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  // List of tech stacks
+  const leftLists = [
+    "ReactJS",
+    "Express",
+    "Typescript",
+    "C++",
+    "Python",
+    "Node.js",
+  ];
+  const rightLists = [
+    "Docker",
+    "NextJS",
+    "GraphQL",
+    "JavaScript",
+    "GitHub",
+    "MongoDB",
+  ];
 
-//   const infiniteScrollVariant = {
-//     animate: {
-//       y: [0, -200], // Moves up by 200px or adjust based on content size
-//     },
-//   };
+  const infiniteScrollVariant = {
+    animate: {
+      y: [0, -200], // Moves up by 200px or adjust based on content size
+    },
+  };
 
-//   const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState(false);
 
-//   const defaultOptions = {
-//     loop: copied,
-//     autoplay: copied,
-//     animationData: animationData,
-//     rendererSettings: {
-//       preserveAspectRatio: "xMidYMid slice",
-//     },
-//   };
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-//   const handleCopy = () => {
-//     const text = "019saxenatushar@gmail.com";
-//     navigator.clipboard.writeText(text);
-//     setCopied(true);
-//   };
+  const handleCopy = () => {
+    const text = "019saxenatushar@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
 
-//   return (
-//     <div
-//       className={cn(
-//         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
-//         className
-//       )}
-//       style={{
-//         background: "rgb(2,0,36)",
-//         backgroundColor:
-//           "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(122,106,192,1) 100%, rgba(0,212,255,1) 100%)",
-//       }}
-//     >
-//       <div className={`${id === 6 && "flex justify-center"} h-full`}>
-//         <div className="w-full h-full absolute">
-//           {img && (
-//             <img
-//               src={img}
-//               alt={img}
-//               className={cn(imgClassName, "object-cover object-center")}
-//             />
-//           )}
-//         </div>
-//         <div
-//           className={`absolute right-0 -bottom-5 ${
-//             id === 5 && "w-full opacity-80"
-//           }`}
-//         >
-//           {spareImg && (
-//             <img
-//               src={spareImg}
-//               alt={spareImg}
-//               className="object-cover object-center w-full h-full"
-//             />
-//           )}
-//         </div>
-//         {id === 6 && (
-//           <BackgroundGradientAnimation>
-//             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-2xl text-center md:text-4xl lg:text-7xl"></div>
-//           </BackgroundGradientAnimation>
-//         )}
+  return (
+    <div
+      className={cn(
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        className
+      )}
+      style={{
+        background: "rgb(2,0,36)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(122,106,192,1) 100%, rgba(0,212,255,1) 100%)",
+      }}
+    >
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+            />
+          )}
+        </div>
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          }`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+        {/* {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-2xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradientAnimation>
+        )} */}
 
-//         <div
-//           className={cn(
-//             titleClassName,
-//             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-//           )}
-//         >
-//           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
-//             {description}
-//           </div>
-//           <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
-//             {title}
-//           </div>
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+          )}
+        >
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
+          </div>
+          <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
+            {title}
+          </div>
 
-//           {id === 1 && <GridGlobe />}
+          {id === 1 && <GridGlobe />}
 
-//           {id === 3 && (
-//             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 px-5">
-//               <motion.div
-//                 className="flex flex-col gap-3 md:gap-3 lg:gap-8"
-//                 variants={infiniteScrollVariant}
-//                 animate="animate"
-//                 transition={{
-//                   duration: 6,
-//                   ease: "linear",
-//                   repeat: Infinity,
-//                 }}
-//               >
-//                 {leftLists.map((item, i) => (
-//                   <span
-//                     key={i}
-//                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//                   >
-//                     {item}
-//                   </span>
-//                 ))}
-//                 {leftLists.map((item, i) => (
-//                   <span
-//                     key={`repeat-left-${i}`}
-//                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//                   >
-//                     {item}
-//                   </span>
-//                 ))}
-//               </motion.div>
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 px-5">
+              <motion.div
+                className="flex flex-col gap-3 md:gap-3 lg:gap-8"
+                variants={infiniteScrollVariant}
+                animate="animate"
+                transition={{
+                  duration: 6,
+                  ease: "linear",
+                  repeat: Infinity,
+                }}
+              >
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                {leftLists.map((item, i) => (
+                  <span
+                    key={`repeat-left-${i}`}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </motion.div>
 
-//               <motion.div
-//                 className="flex flex-col gap-3 md:gap-3 lg:gap-8"
-//                 variants={infiniteScrollVariant}
-//                 animate="animate"
-//                 transition={{
-//                   duration: 6,
-//                   ease: "linear",
-//                   repeat: Infinity,
-//                 }}
-//               >
-//                 {rightLists.map((item, i) => (
-//                   <span
-//                     key={i}
-//                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//                   >
-//                     {item}
-//                   </span>
-//                 ))}
-//                 {rightLists.map((item, i) => (
-//                   <span
-//                     key={`repeat-right-${i}`}
-//                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//                   >
-//                     {item}
-//                   </span>
-//                 ))}
-//               </motion.div>
-//             </div>
-//           )}
+              <motion.div
+                className="flex flex-col gap-3 md:gap-3 lg:gap-8"
+                variants={infiniteScrollVariant}
+                animate="animate"
+                transition={{
+                  duration: 6,
+                  ease: "linear",
+                  repeat: Infinity,
+                }}
+              >
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                {rightLists.map((item, i) => (
+                  <span
+                    key={`repeat-right-${i}`}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </motion.div>
+            </div>
+          )}
 
-//           {id === 6 && (
-//             <div className="mt-5 relative">
-//               <div
-//                 className={`absolute -bottom-5 right-0 ${
-//                   copied ? "block" : "block"
-//                 }`}
-//               >
-//                 <Lottie options={defaultOptions} height={200} width={400} />
-//               </div>
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
+              >
+                <Lottie options={defaultOptions} height={200} width={400} />
+              </div>
 
-//               <MagicButton
-//                 title={copied ? "Email is Copied!" : "Copy my email address"}
-//                 icon={<IoCopyOutline />}
-//                 position="left"
-//                 handleClick={handleCopy}
-//                 otherClasses="!bg-[#161A31]"
-//               />
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+              <MagicButton
+                title={copied ? "Email is Copied!" : "Copy my email address"}
+                icon={<IoCopyOutline />}
+                position="left"
+                handleClick={handleCopy}
+                otherClasses="!bg-[#161A31]"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
