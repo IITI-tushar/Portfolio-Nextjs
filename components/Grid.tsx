69c9e0ae34f17205eb1a2cb846602581d117ed53
IITI-tushar <019saxenatushar@gmail.com>
2025-01-12 00:24:24 +0530

    update11

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,7 +6,7 @@ const Grid = () => {
   return (
     <section id="about">
       {/* <BentoGrid className="w-full py-20"> */}
-      {gridItems.map((item, i) => (
+      {/* {gridItems.map((item, i) => (
         <BentoGridItem
           id={item.id}
           key={i}
@@ -18,7 +18,7 @@ const Grid = () => {
           titleClassName={item.titleClassName}
           spareImg={item.spareImg}
         />
-      ))}
+      ))} */}
       {/* </BentoGrid> */}
     </section>
   );
